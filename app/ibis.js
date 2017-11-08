65dd998923ac98dbe23d78462ce2076dbce3285d
lucyq <lucyzqin@gmail.com>
2017-11-08 14:28:23 -0500

    trying post request

--- app/ibis.js
+++ app/ibis.js
@@ -24,9 +24,11 @@ var grammer_def = [
 var sample_str = "one-or-more (one-or-more ( prom_f then spacer_f) then cds_f then zero-or-more (spacer_f or one-or-more (spacer_f then prom_f then spacer_f) then cds_f) then term_f or (term_f then spacer_f) or (spacer_f then term_f))"
 
 
-var parsed = '{"OneOrMore":[{"Then":[{"OneOrMore":[{"Then":[{"Atom":["prom_f"]},{"Atom":["spacer_f"]}]}]},{"Then":[{"Atom":["cds_f"]},{"Then":[{"ZeroOrMore":[{"Then":[{"Or":[{"Atom":["spacer_f"]},{"OneOrMore":[{"Then":[{"Atom":["spacer_f"]},{"Then":[{"Atom":["prom_f"]},{"Atom":["spacer_f"]}]}]}]}]},{"Atom":["cds_f"]}]}]},{"Or":[{"Atom":["term_f"]},{"Or":[{"Then":[{"Atom":["term_f"]},{"Atom":["spacer_f"]}]},{"Then":[{"Atom":["spacer_f"]},{"Atom":["term_f"]}]}]}]}]}]}]}]}';
+// var parsed = '{"OneOrMore":[{"Then":[{"OneOrMore":[{"Then":[{"Atom":["prom_f"]},{"Atom":["spacer_f"]}]}]},{"Then":[{"Atom":["cds_f"]},{"Then":[{"ZeroOrMore":[{"Then":[{"Or":[{"Atom":["spacer_f"]},{"OneOrMore":[{"Then":[{"Atom":["spacer_f"]},{"Then":[{"Atom":["prom_f"]},{"Atom":["spacer_f"]}]}]}]}]},{"Atom":["cds_f"]}]}]},{"Or":[{"Atom":["term_f"]},{"Or":[{"Then":[{"Atom":["term_f"]},{"Atom":["spacer_f"]}]},{"Then":[{"Atom":["spacer_f"]},{"Atom":["term_f"]}]}]}]}]}]}]}]}';
 
-// var parsed = '{"Or": [{"Then": [{"Atom": ["term_f"]}, {"Atom": ["spacer_f"]}]},{"Then": [{"Atom": ["spacer_f"]}, {"Atom": ["term_f"]}]}]}'
+var parsed = '{"Or": [{"Then": [{"Atom": ["term_f"]}, {"Atom": ["spacer_f"]}]},{"Then": [{"Atom": ["spacer_f"]}, {"Atom": ["term_f"]}]}]}'
+
+var parsed = '{"And": [{"Atom": ["a"]}, {"Atom": ["b"]}]}'
 
 function init() {
 
@@ -43,10 +45,12 @@ function init() {
     });
 
     parsed = JSON.parse(parsed);
+
+    console.log(parsed);
     g.addVertex('root', []);    
     traverse(parsed, 'root', g);
     enumerateDesigns(g);
-    // console.log(g);
+ 
     
 }
 
@@ -65,16 +69,15 @@ function traverse(obj, prevNode, g) {
 }
 
 function enumerateDesigns(g) {
-    console.log(g);
+    // console.log(g.snapshot());
 }
 
 function handleOperator(op, d, prevNode, g) {
-
+    // console.log(op, d)
     var id = uuidv4();
     if (op === "Or" || op === "Then" || op === "And" || op === "Atom") {
-        g.addVertex(id, []);
-        g.addEdge(prevNode, id, []);
-        
+        g.addVertex(id, {"labels": op, "data": d});
+        g.addEdge(prevNode, id); 
     }
 
     if (op === "OneOrMore") {
@@ -82,6 +85,10 @@ function handleOperator(op, d, prevNode, g) {
         g.addEdge(id, id, []);
     }
     
+    if (op === "ZeroOrMore") {
+
+    }
+
     return id;
 
 
--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const http = require('http')
-const uuidv4 = require('uuid/v4');
 const ibis = require('./app/ibis')
+const bodyParser = require("body-parser");
 
 
 
@@ -14,13 +14,20 @@ var server = app.listen(8081, function() {
     console.log('Ready on port %d', server.address().port);
 });
 
+app.use(bodyParser.json({limit: '50mb'}));
 
-app.use(express.static(__dirname + '/app'));
+
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(req, res) {
-    res.sendFile((path.join(__dirname + '/app/index.html')));
+    res.sendFile((path.join(__dirname + '/static/index.html')));
 });
 
+app.post('/postLangText', function(req,res) {
+    console.log('q',req);
+    // req.body
+    res.send('POST lang')
+});
 
 ibis.ibis();
 
