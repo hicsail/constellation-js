0a51caceb3fb1e29ff7ad24de891f3a025cf74cb
lucyq <lucyzqin@gmail.com>
2017-11-16 17:56:21 -0500

    or functioning with multiple roots as children

---
+++ tests/test.js
@@ -0,0 +1,12 @@
+// Atomic statements
+(a or b)
+(a and b)
+(a then b)
+(zero-or-more a)
+(one-or-more a)
+
+// Chained atomic statements
+((a or b) or c)
+((a or b) or (c or d))
+((a and b) and c)
+
