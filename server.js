e5eccc2a90f776acf2e4f82ccaa3f6707c4b95a8
lucyq <lucyzqin@gmail.com>
2017-11-18 12:36:22 -0500

    commented out server since functionality is on client side. also cleaned up repo

--- server.js
+++ server.js
@@ -1,34 +1,34 @@
-'use strict'
+// 'use strict'
 
 
-const express = require('express');
-const app = express();
-const path = require('path');
-const http = require('http')
-const ibis = require('./app/ibis')
-const bodyParser = require("body-parser");
+// const express = require('express');
+// const app = express();
+// const path = require('path');
+// const http = require('http')
+// const ibis = require('./app/ibis')
+// const bodyParser = require("body-parser");
 
 
 
-var server = app.listen(8081, function() {
-    console.log('Ready on port %d', server.address().port);
-});
+// var server = app.listen(8081, function() {
+//     console.log('Ready on port %d', server.address().port);
+// });
 
-app.use(bodyParser.json({limit: '50mb'}));
+// app.use(bodyParser.json({limit: '50mb'}));
 
 
-app.use(express.static(__dirname + '/static'));
+// app.use(express.static(__dirname + '/static'));
 
-app.get('/', function(req, res) {
-    res.sendFile((path.join(__dirname + '/static/index.html')));
-});
+// app.get('/', function(req, res) {
+//     res.sendFile((path.join(__dirname + '/static/index.html')));
+// });
 
-app.post('/postLangText', function(req,res) {
-    console.log('q',req);
-    // req.body
-    res.send('POST lang')
-});
+// // app.post('/postLangText', function(req,res) {
+// //     console.log('q',req);
+// //     // req.body
+// //     res.send('POST lang')
+// // });
 
-ibis.ibis();
+// ibis.ibis();
 
 
