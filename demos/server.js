b46c185fe5ba05fec1985b59cb562bccbb8349fc
Andrei Lapets <a@lapets.io>
2018-02-11 15:50:34 -0500

    Allow client page to access library files.

--- demos/server.js
+++ demos/server.js
@@ -19,6 +19,7 @@ app.use(function(req, res, next) {
 app.use(express.static(__dirname + '/static'));
 app.use(express.static(__dirname + '/static/css'));
 app.use(express.static(__dirname + '/static/js'));
+app.use(express.static(__dirname + '/../lib'));
 
 var server = app.listen(8082, function() {
   console.log('Listening on port %d', server.address().port);
