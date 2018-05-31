819c5f943d1ea2d2d18c25fab8a9c26819afdce8
gxavier38 <gxavier38@gmail.com>
2018-05-31 16:54:10 -0400

    Added unit tests

--- test/constellationTests.js
+++ test/constellationTests.js
@@ -5,66 +5,105 @@ const ATOM = 'atom';
 const expect = require('chai').expect;
 const constellation = require('../lib/constellation');
 
-// var categories = '{"promoter": ["BBa_R0040", "BBa_J23100"], "rbs": ["BBa_B0032", "BBa_B0034"], "gene": ["BBa_E0040", "BBa_E1010"], "terminator": ["BBa_B0010"]}';
-let categories = '{"a":["a1","a2"],"b":["b1","b2","b3"],"c":["c1"],"d":["d1"]}';
+let categories = '{"a":["a1","a2"],"b":["b1","b2","b3"],"c":["c1"]}';
 categories = JSON.parse(categories);
 
 module.exports = function() {
 
-  describe('#constellation', function() {
-  // BASIC OPERATORS
+  describe('Specification parsing', function() {
     it('atom', function() {
       let result = constellation('c', categories, 10);
       expect(result.designs[0]).to.equal('c1');
     });
 
     it('or', function() {
-      categories = {'a': ['a1', 'a2'], 'b': ['b']};
-
+      let categories = {'a': ['a1', 'a2'], 'b': ['b']};
       const result = constellation('a or b', categories, 10);
       const len = categories['a'].length + categories['b'].length;
       expect(result.designs.length).to.equal(len);
+      expect(result.designs).to.contain('b');
+      expect(result.designs).to.contain('a1');
+      expect(result.designs).to.contain('a2');
     });
 
     it('then', function() {
-      categories = {'a': ['a1', 'a2'], 'b': ['b']};
-
-      const result = constellation('a . b', categories, 10);
-      const len = categories['a'].length * categories['b'].length;
-      expect(result.designs.length).to.equal(len);
+      const result = constellation('a then c', categories, 10);
+      expect(result.designs.length).to.equal(2);
+      expect(result.designs).to.contain('a1,c1');
+      expect(result.designs).to.contain('a2,c1');
     });
 
-    // it('zero-or-more', function() {
-    //     var result = constellation('zero-or-more a', categories, 10);
-    //     var paths = result.paths;
-    //     expect(paths.length).to.equal(2);
-    // });
+    it('then as dot', function() {
+      const result = constellation('a . c', categories, 10);
+      expect(result.designs.length).to.equal(2);
+      expect(result.designs).to.contain('a1,c1');
+      expect(result.designs).to.contain('a2,c1');
+    });
 
+    it('and', function() {
+      const result = constellation('(a or c) and a', categories, 10);
+      expect(result.designs.length).to.equal(1);
+      expect(result.designs).to.contain('a1');
+      expect(result.designs).to.contain('a2');
+    });
 
-    // it('one-or-more', function() {
-    //     var result = constellation('one-or-more a', categories, 10);
-    //     var paths = result.paths;
+    it('one-or-more', function() {
+        let result = constellation('one-or-more a', categories, 10);
+        expect(result.designs).to.contain('a1');
+        expect(result.designs).to.contain('a2');
+        expect(result.paths.length).to.equal(1); // Number of paths
+        expect(result.paths[0].length).to.equal(2); // Length of each path, including root
+        expect(result.paths[0][0].dataType === ATOM);
+    });
 
-    //     expect(paths.length).to.equal(1);
-    //     expect(paths[0].length).to.equal(1);
-    //     expect(paths[0][0].dataType === ATOM)
-    // });
+    it('zero-or-more', function() {
+      const result = constellation('zero-or-more a', categories, 10);
+      console.log(result.designs);
+      expect(result.designs).to.contain('');
+      expect(result.designs).to.contain('a1');
+      expect(result.designs).to.contain('a2');
+      expect(result.paths.length).to.equal(2); // Number of paths, including empty path
+      expect(result.paths[0][0].dataType === ATOM);
+    });
 
+  });
 
-    // // CHAINED EXPRESSIONS
-    // it('multiple then', function() {
-    //     var result = constellation('a . b . c', categories, 10);
-    //     expect(result.designs.length).to.equal(categories['a'].length * categories['b'].length * categories['c'].length);
-    //     expect(result.paths.length).to.equal(1);
-    // });
+  describe ('Chained expressions', function() {
+    it('Multiple then', function() {
+        const result = constellation('a then b then c', categories, 10);
+        expect(result.designs.length).to.equal(categories['a'].length * categories['b'].length * categories['c'].length);
+    });
+  });
 
-    // BASIC ERRORS
-    it ('basic errors', function() {
+  describe('Sanitise specification input', function () {
+    it('Atom not in categories', function () {
       const result = constellation('d', categories, 10);
       expect(JSON.stringify(result.designs)).to.include('Error');
+      expect(JSON.stringify(result.designs)).to.include('not in part categories');
     });
 
-  // PARSING ERROR!!
+    it('Mismatched brackets', function () {
+      expect(constellation('(a}', categories, 10)).to.throw(Error);
+    });
+
+    describe('Invalid characters', function () {
+      it('Tabs used should not throw errors', function () {
+        const result = constellation('\ta', categories, 10);
+        expect(result.designs[0]).to.equal(['a1', 'a2']);
+      });
+
+      it('$', function () {
+        const result = constellation('$a', categories, 10);
+        expect(JSON.stringify(result.designs)).to.include('Error');
+      });
+    });
+
+  });
+
+  describe('Sanitise category input', function () {
+    it ('', function () {
+
+    });
   });
 
 };
--- test/enumerationTests.js
+++ test/enumerationTests.js
@@ -19,30 +19,23 @@ function generateAtom() {
 }
 
 module.exports = function() {
-  describe('#design-enumeration', function() {
-    it('atom', function() {
+  describe('Design enumeration', function() {
+    it('Enumerate designs for one atom', function() {
       const collection = {'a': ['a']};
       const paths = [[generateRoot(), generateAtom()]];
       const designs = enumeration(paths, collection, 1);
       expect(JSON.stringify(designs)).to.equal(JSON.stringify(collection['a']));
     });
 
-    it('empty-part', function() {
-      const collection = {'a': []};
-      const paths = [[generateRoot(), generateAtom()]];
-      const designs = enumeration(paths, collection, 1);
-      expect(JSON.stringify(designs)).to.equal(JSON.stringify(collection['a']));
-    });
-
-    it('missing-part', function() {
-      const collection = {'b': []};
+    it('Handle empty categories', function() {
+      const collection = {'d': []};
       const paths = [[generateRoot(), generateAtom()]];
       const designs = enumeration(paths, collection, 1);
       expect(JSON.stringify(designs)).to.contain('Error');
       expect(designs.length).to.equal(1);
     });
 
-    it('duplicates', function() {
+    it('Enumerate all duplicates in category', function() {
       const collection = {'a': ['a1', 'a2']};
       const path = [generateRoot(), generateAtom()];
       const paths = [path, path];
@@ -50,19 +43,28 @@ module.exports = function() {
       expect(JSON.stringify(designs)).to.equal(JSON.stringify(collection['a']));
     });
 
-    // NUM DESIGNS
-    it('select 1 of 2 designs', function() {
-      const collection = {'a': ['a1', 'a2']};
-      const paths = [[generateRoot(), generateAtom()]];
-      const designs = enumeration(paths, collection, 1);
-      expect(designs.length).to.equal(1);
+    it('Multi-level graphs', function() {
+      const collection = {'a': ['a1', 'a2'], 'b': ['b1']};
+      const path = [generateRoot(), generateAtom()];
+      const paths = [path, path];
+      const designs = enumeration(paths, collection, 4);
+
     });
 
-    it('select 0 designs', function() {
-      const collection = {'a': ['a1', 'a2']};
-      const paths = [[generateRoot(), generateAtom()]];
-      const designs = enumeration(paths, collection, 0);
-      expect(designs.length).to.equal(0);
+    describe ('Number of designs chosen', function() {
+      it('Select 1 of 2 designs', function () {
+        const collection = {'a': ['a1', 'a2']};
+        const paths = [[generateRoot(), generateAtom()]];
+        const designs = enumeration(paths, collection, 1);
+        expect(designs.length).to.equal(1);
+      });
+
+      it('Select 0 designs', function () {
+        const collection = {'a': ['a1', 'a2']};
+        const paths = [[generateRoot(), generateAtom()]];
+        const designs = enumeration(paths, collection, 0);
+        expect(designs.length).to.equal(0);
+      });
     });
   });
 };
