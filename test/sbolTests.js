78ca5576491d85944109ffe83f909e79adfa9ddb
lucyq <lucyzqin@gmail.com>
2019-01-18 10:37:35 -0500

    comparing SBOL examples to text files instead of blocks of text

--- test/sbolTests.js
+++ test/sbolTests.js
@@ -1,7 +1,8 @@
 
 const constellation = require('../lib/constellation');
 const expect = require('chai').expect;
-const fs = require('fs');
+var fs = require('fs');
+
 
 var DESIGN_NAME = 'design';
 const CATEGORIES = '{"rbs":["a1","a2"],"cds":["b1","b2","b3"],"promoter":["c1"], "terminator": ["t"]}';
@@ -12,31 +13,37 @@ function trim(str) {
 }
 
 
+function readModuleFile(path, callback) {
+  try {
+      var filename = require.resolve(path);
+      fs.readFile(filename, 'utf8', callback);
+  } catch (e) {
+    console.log('error?', e);
+      callback(e);
+  }
+}
+
 module.exports = function() {
-//
-describe('SBOL Generation', function() {
-  it('atom', function() {
-    let result = constellation(DESIGN_NAME, 'rbs', CATEGORIES, 10, 0);
-    
-    fs.readFile('spec.txt', (err, data) => {
-      if (err) throw err;
+  //
+  describe('SBOL Generation', function() {
+    it('atom', function(done) {
+      let result = constellation(DESIGN_NAME, 'rbs', CATEGORIES, 10, 0);
 
-      expect(trim(result.sbols[0])).to.equal(data.toString());
+      readModuleFile('./sbolResults/atom.txt', function (err, words) {
+        expect(err).to.be.a('null');
+        expect(trim(result.sbols[0])).to.eql(trim(words));
+        done();
+      });
+    });
 
+    it('Spec for paper', function() {
+      let result = constellation(DESIGN_NAME, 'one-or-more(one-or-more(promoter then cds)then cds then (zero-or-more (cds or (one-or-more (cds then promoter then cds) then cds)) then (terminator or (terminator then cds) or (cds then terminator)))))', CATEGORIES, 10, 0);
+  
+      readModuleFile('./sbolResults/paperEx.txt', function (err, words) {
+        expect(err).to.be.a('null');
+        expect(trim(result.sbols[0])).to.eql(trim(words));
+        done();
+      });
     });
   });
-
-  // it('Spec for paper', function() {
-  //   let result = constellation(DESIGN_NAME, 'one-or-more(one-or-more(promoter then cds)then cds then (zero-or-more (cds or (one-or-more (cds then promoter then cds) then cds)) then (terminator or (terminator then cds) or (cds then terminator)))))', CATEGORIES, 10, 0);
-
-    
-
-  //   fs.readFile('spec.txt', (err, data) => {
-  //     if (err) throw err;
-  //     console.log(data.toString());
-  //   });
-
-  // });
-});
-
 }
