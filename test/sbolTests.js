d27fb93c82db642c3d94ecaeb8c58d1cf11f83db
Dany Fu <danyfu@bu.edu>
2019-02-05 14:06:18 -0500

    trim whitespace from SBOL unit tests

--- test/sbolTests.js
+++ test/sbolTests.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 var DESIGN_NAME = 'design';
 const CATEGORIES = '{"rbs":["a1","a2"],"cds":["b1","b2","b3"],"promoter":["c1"], "terminator": ["t"]}';
 
-function trim(str) {
+function trimX(str) {
   return str.replace(/\s/g, "X");
 
 }
@@ -30,17 +30,17 @@ module.exports = function() {
 
       readModuleFile('./sbolResults/atom.txt', function (err, words) {
         expect(err).to.be.a('null');
-        expect(trim(result.sbols[0])).to.eql(trim(words));
+        expect(trimX(result.sbols[0].trim())).to.eql(trimX(words.trim()));
         done();
       });
     });
 
     it('Spec for paper', function() {
       let result = constellation(DESIGN_NAME, 'one-or-more(one-or-more(promoter then cds)then cds then (zero-or-more (cds or (one-or-more (cds then promoter then cds) then cds)) then (terminator or (terminator then cds) or (cds then terminator)))))', CATEGORIES, 10, 0);
-  
+
       readModuleFile('./sbolResults/paperEx.txt', function (err, words) {
         expect(err).to.be.a('null');
-        expect(trim(result.sbols[0])).to.eql(trim(words));
+        expect(trimX(result.sbols[0].trim())).to.eql(trimX(words.trim()));
         done();
       });
     });
