97c9cf0aada1a7ea7b433bd75567040364653e86
Dany Fu <danyfu@bu.edu>
2019-02-21 15:18:15 -0500

    add numDesigns and maxCycle to SBOL as custom attributes

--- test/sbolTests.js
+++ test/sbolTests.js
@@ -3,7 +3,8 @@ const constellation = require('../lib/constellation');
 const expect = require('chai').expect;
 let fs = require('fs');
 
-
+let NUM_DESIGNS = 10;
+let MAX_CYCLES = 0;
 let DESIGN_NAME = 'design';
 const CATEGORIES = '{"rbs":["a1","a2"],"cds":["b1","b2","b3"],"promoter":["c1"], "terminator": ["t"]}';
 
@@ -26,7 +27,7 @@ module.exports = function() {
 
   describe('SBOL Generation', function() {
     it('atom', function(done) {
-      let result = constellation.constellationGOLDBAR(DESIGN_NAME, 'rbs', CATEGORIES, 10, 0);
+      let result = constellation.constellationGOLDBAR(DESIGN_NAME, 'rbs', CATEGORIES, NUM_DESIGNS, MAX_CYCLES);
 
       readModuleFile('./sbolResults/atom.txt', function (err, words) {
         expect(err).to.be.a('null');
@@ -37,7 +38,7 @@ module.exports = function() {
 
     it('Spec for paper', function(done) {
       const spec = 'one-or-more(one-or-more(promoter then cds)then cds then (zero-or-more (cds or (one-or-more (cds then promoter then cds) then cds)) then (terminator or (terminator then cds) or (cds then terminator)))))';
-      let result = constellation.constellationGOLDBAR(DESIGN_NAME, spec, CATEGORIES, 10, 0);
+      let result = constellation.constellationGOLDBAR(DESIGN_NAME, spec, CATEGORIES, NUM_DESIGNS, MAX_CYCLES);
 
       readModuleFile('./sbolResults/paperEx.txt', function (err, words) {
         expect(err).to.be.a('null');
