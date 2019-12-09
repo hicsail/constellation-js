3ec9356b416683e175777e3195f3a65917159f7f
Vidya Akavoor <vidyaap@bu.edu>
2019-12-09 15:09:15 -0500

    Remove config file

--- demos/static/js/client.js
+++ demos/static/js/client.js
@@ -1,11 +1,10 @@
-import {config} from './config.js';
 const LINKDISTANCE = 25;
 const CHARGESTRENGTH = -400;
 const MAXDISTANCE = 100;
 const IMAGESIZE = 30;
 const RADIUS = 7;
 const INTERMEDIATE = 'intermediate';
-const REPRESENTATION = config.representation;
+const REPRESENTATION = 'EDGE';
 
 let nodePointer, linkPointer, simulationPointer, svgPointer, circlePointer, imagePointer, textPointer, width, height, sbolDoc, sbolFile;
 let designName = 'Constellation';
