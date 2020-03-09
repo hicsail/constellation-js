1bb80f6a55bf8953f831a1cc2b09f323a63fed17
Vidya Akavoor <vidyaap@bu.edu>
2020-03-09 15:58:03 -0400

    Add unit tests for edge SBOL

--- test/sbolTests.js
+++ test/sbolTests.js
@@ -7,6 +7,9 @@ const CATEGORIES = '{"rbs":{"rbs":["a1","a2"]},"cds":{"cds":["b1","b2","b3"]},"p
 const NODE = 'NODE';
 const NODE_REP = {designName: 'design', representation:NODE};
 
+const EDGE = 'NODE';
+const EDGE_REP = {designName: 'design', representation:EDGE};
+
 function trimX(str) {
   return str.replace(/\s/g, "X");
 }
@@ -24,7 +27,7 @@ module.exports = function() {
 
   describe('SBOL Generation', function() {
 
-    describe('Unary expressions', function() {
+    describe('Unary expressions NODE', function() {
       it('atom', async() => {
         let result = await constellation.goldbar('rbs', CATEGORIES, NODE_REP);
 
@@ -55,7 +58,38 @@ module.exports = function() {
       });
     });
 
-    describe('Binary expressions', function() {
+    describe('Unary expressions EDGE', function() {
+      it('atom', async() => {
+        let result = await constellation.goldbar('rbs', CATEGORIES, EDGE_REP);
+
+        readModuleFile('./sbolResults/atom.txt', function (err, words) {
+          expect(err).to.be.a('null');
+          expect(trimX(result.sbol.trim())).to.eql(trimX(words.trim()));
+        });
+      });
+
+      it('one-or-more', async()=> {
+        const spec = 'one-or-more rbs';
+        let result = await constellation.goldbar(spec, CATEGORIES, EDGE_REP);
+
+        readModuleFile('./sbolResults/one-or-more.txt', function (err, words) {
+          expect(err).to.be.a('null');
+          expect(trimX(result.sbol.trim())).to.eql(trimX(words.trim()));
+        });
+      });
+
+      it('zero-or-more', async() => {
+        const spec = 'zero-or-more rbs';
+        let result = await constellation.goldbar(spec, CATEGORIES, EDGE_REP);
+
+        readModuleFile('./sbolResults/zero-or-more.txt', function (err, words) {
+          expect(err).to.be.a('null');
+          expect(trimX(result.sbol.trim())).to.eql(trimX(words.trim()));
+        });
+      });
+    });
+
+    describe('Binary expressions NODE', function() {
 
       it('or', async() => {
         const spec = 'promoter or rbs';
@@ -70,6 +104,29 @@ module.exports = function() {
       it('then', async() => {
         const spec = 'promoter then rbs';
         let result = await constellation.goldbar(spec, CATEGORIES, NODE_REP);
+
+        readModuleFile('./sbolResults/then.txt', function (err, words) {
+          expect(err).to.be.a('null');
+          expect(trimX(result.sbol.trim())).to.eql(trimX(words.trim()));
+        });
+      });
+    });
+
+    describe('Binary expressions EDGE', function() {
+
+      it('or', async() => {
+        const spec = 'promoter or rbs';
+        let result = await constellation.goldbar(spec, CATEGORIES, EDGE_REP);
+
+        readModuleFile('./sbolResults/or.txt', function (err, words) {
+          expect(err).to.be.a('null');
+          expect(trimX(result.sbol.trim())).to.eql(trimX(words.trim()));
+        });
+      });
+
+      it('then', async() => {
+        const spec = 'promoter then rbs';
+        let result = await constellation.goldbar(spec, CATEGORIES, EDGE_REP);
 
         readModuleFile('./sbolResults/then.txt', function (err, words) {
           expect(err).to.be.a('null');
@@ -99,6 +156,36 @@ module.exports = function() {
     it('Parse SBOL repeat', async() => {
       let result = await constellation.goldbar('promoter then zero-or-more rbs then cds', CATEGORIES, NODE_REP);
       result = await constellation.sbol([result.sbol], '', 0, NODE);
+      let atomTexts = Object.values(result.stateGraph).map(obj => obj.text).sort();
+      expect(atomTexts).to.be.an('array').that.includes('promoter');
+      expect(atomTexts).to.be.an('array').that.includes('rbs');
+      expect(atomTexts).to.be.an('array').that.includes('cds');
+
+        let operators = [];
+        for (let value of Object.values(result.stateGraph)){
+          operators.push(...value.operator);
+        }
+        operators.sort();
+        expect(operators).to.deep.eql(['Then', 'Then', 'ZeroOrMore']);
+    });
+
+    it('Parse SBOL or', async() => {
+      let result = await constellation.goldbar('promoter or cds', CATEGORIES, EDGE_REP);
+      result = await constellation.sbol([result.sbol],'', 0, EDGE);
+      let atomTexts = Object.values(result.stateGraph).map(obj => obj.text).sort();
+      expect(atomTexts).to.be.an('array').that.includes('promoter');
+      expect(atomTexts).to.be.an('array').that.includes('cds');
+
+      let operators = [];
+      for (let value of Object.values(result.stateGraph)){
+        operators.push(...value.operator);
+      }
+      expect(operators).to.deep.eql(['Or']);
+    });
+
+    it('Parse SBOL repeat', async() => {
+      let result = await constellation.goldbar('promoter then zero-or-more rbs then cds', CATEGORIES, EDGE_REP);
+      result = await constellation.sbol([result.sbol], '', 0, EDGE);
       let atomTexts = Object.values(result.stateGraph).map(obj => obj.text).sort();
       expect(atomTexts).to.be.an('array').that.includes('promoter');
       expect(atomTexts).to.be.an('array').that.includes('rbs');
